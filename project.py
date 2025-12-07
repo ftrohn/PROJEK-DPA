600d8ebd024c3c87f494ffa14eb682f1bbe4f854
ftrohn <fitrohnura2310@gmail.com>
2025-12-07 20:49:35 +0700

    commit fitroh

--- project.py
+++ project.py
@@ -1,96 +1,54 @@
+## Storage ##
+inventory = {}
+isi_file = None
 
-# Menampilkan Isi Toko
-inventory = []
+## membuat inventory baru dan file baru ##
+def buat_inventory_baru():
+    global isi_file
+    inventory.clear()
 
-def tampilkan_inventory():
-    print("\n================= DAFTAR INVENTORY ==================")
-    if not inventory:
-        print("\n[!] Inventory masih kosong.\n")
-    else:
-        for i, data_inventory in enumerate(inventory, 1):
-            print(f"{i}. {data_inventory['nama']} | Stok: {data_inventory['Stok']} Kg | Harga: Rp{data_inventory['Harga']:,.0f}")
-    print("=====================================================\n")
+    nama_file = input("Masukkan nama file baru (tanpa .txt) : ").strip()
 
-#Menu Tambah Barang
-def tambah_barang():
-    print("\n--- Tambah Barang Baru ---")
-    nama_barang = input("Masukkan Nama Buah: ").strip().capitalize()
-
-    # Logika Cek Duplikat untuk List of Dictionaries
-    if any(item.get('nama') == nama_barang for item in inventory):
-        print(f"[!] Buah '{nama_barang}' sudah ada. Silakan gunakan menu Update Stok.")
-        return
-        
     try:
-        stok_barang = int(input("Masukkan Jumlah Stok (kg): "))
-        harga_barang = float(input("Masukkan Harga Satuan (Rp): "))
-
-        if stok_barang < 0 or harga_barang < 0:
-            print("[!] Stok dan Harga tidak boleh negatif.")
-            return
-
-        data_baru = {
-            'nama': nama_barang,
-            'Stok': stok_barang, 
-            'Harga': harga_barang
-        }
-        
-        inventory.append(data_baru)
-        print(f"\n[✓] Barang '{nama_barang}' berhasil ditambahkan ke inventory.")
-
-    except ValueError:
-        print("\n[!] Input tidak valid. Pastikan Stok dan Harga adalah angka.")
-
-
-### MENU UPDATE STOK ###
-def update_stok():
-    """Update tok barang"""
-    tampilkan_inventory()
-    if not inventory:
+        with open(f"{nama_file}.txt", "x") as file:
+            pass
+    except FileExistsError:
+        print(f"[!] file '{nama_file}.txt' sudah ada. data tidak tersimpan\n")
         return
-    
-    pilihan = input("Pilih nomor barang: ").strip() 
-    stok_baru = input("Masukkan stok baru: ").strip()
 
-    if pilihan.isdigit() and stok_baru.isdigit():
-        pilihan = int(pilihan)
-        stok_baru = int(stok_baru)
-
-        if 1 <= pilihan <= len(inventory):
-            inventory[pilihan -1]["Stok"] = stok_baru
-            nama_barang = inventory[pilihan -1]["nama"]
-            print(f"Stok {nama_barang} berhasil diupdate menjadi {stok_baru} Kg")
-        else:
-            print("UPS, Nomor barang tidak valid")
-    else:
-        print("Input Harus Angka woiiiiiii")
-
-
-### LOOPING - Menu utama ###
-def main_menu():
     while True:
-        print("="*40)
-        print("SISTEM MANAJEMEN INVENTORY TOKO BUAH")
-        print("="*40)
-        print("1. Tampilkan Inventory")
-        print("2. Tambah Barang")
-        print("3. Update Stok")
-        print("4. Keluar")
-        
-        pilihan = input("Pilih menu (1-4): ")
-        
-        if pilihan == '1':
-            tampilkan_inventory()
-        elif pilihan == '2':
-            tambah_barang()
-        elif pilihan == '3':
-            update_stok()
-        elif pilihan == '4':
-            print("Terima kasih! Program selesai.")
-            break
-        else:
-            print("Pilihan tidak valid!")
+        try:
+            nama = input("Masukkan nama barang : ").strip().capitalize()
+            stok = int(input("Masukkan stok barang : "))
+            satuan = input("Masukkan satuan barang (buah, pcs, kg, dll) : ").strip().lower()
 
-# Jalankan program
-if __name__ == "__main__":
-    main_menu()
+            if stok < 0:
+                print("[!] stok tidak boleh negatif\n")
+                continue
+
+        except ValueError:
+            print("[!] stok harus berupa angka\n")
+            continue
+
+        if nama in inventory:
+            print(f"Barang '{nama}' sudah ada, silakan masukkan nama lain\n")
+            continue
+        else:
+            inventory[nama] = (stok, satuan)
+            print(f"Barang '{nama}' berhasil ditambahkan")
+
+        lanjut = input("Tambah barang lagi? (y/n): ").strip().lower()
+        print()
+        if lanjut != "y":
+            break
+
+    if not inventory:
+        print("Tidak ada data yang disimpan")
+        return
+
+    with open(f"{nama_file}.txt", "w") as file:
+        for nama, (stok, satuan) in inventory.items():
+            file.write(f"{nama},{stok},{satuan}\n")
+
+    isi_file = nama_file
+    print(f"Data berhasil disimpan di {nama_file}.txt")
